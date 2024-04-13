0e154cc30e04a22b12a8ed58230f8dd7be17f8d7
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-04-13 15:39:05 +1000

    Fixed black colour constant

--- config/constants.py
+++ config/constants.py
@@ -3,7 +3,7 @@ import pygame
 
 # Colour constants
 WHITE = pygame.Color(255, 255, 255)
-BLACK = pygame.Color(255, 255, 255)
+BLACK = pygame.Color(0, 0, 0)
 RED = pygame.Color(255, 0, 0)
 YELLOW = pygame.Color(255, 255, 0)
 GREEN = pygame.Color(0, 255, 0)
